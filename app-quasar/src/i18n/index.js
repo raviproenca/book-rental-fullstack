4b4bf133cb43699cd1b80dcd34178ef3a1093831
Ravi Ribeiro Proença <raviproenca@gmail.com>
2025-11-18 10:22:41 -0300

    Fix: Corrigindo nomes das pastas i18n de pt-BR para ptBR

--- app-quasar/src/i18n/index.js
+++ app-quasar/src/i18n/index.js
@@ -1,9 +1,9 @@
-import enUS from './en-US'
-import esES from './es-ES'
-import ptBR from './pt-BR'
+import enUS from './enUS'
+import esES from './esES'
+import ptBR from './ptBR'
 
 export default {
   'en-US': enUS,
-  'pt-BR': ptBR,
   'es-ES': esES,
+  'pt-BR': ptBR,
 }
